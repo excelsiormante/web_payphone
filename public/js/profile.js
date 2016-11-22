d051370df6c4e496400669c499de0c62dce41572
Joshua Mante <excelsior.mante@gmail.com>
2016-11-22 13:58:37 +0800

    Merge
    Signed-off-by: Joshua Mante <excelsior.mante@gmail.com>

--- public/js/profile.js
+++ public/js/profile.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
         // process the form
         $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
             url         : 'edit/profile', // the url where we want to POST
             data        : formData, // our data object
             dataType    : 'json', // what type of data do we expect back from the server
